f8f05db507ae0c1f46c3a6d18fe3527d07df49c6
SaadUddin <saad.uddin9900@gmail.com>
2024-11-12 09:52:32 -0800

    Home page working
    Pushed

--- saas_landingpage_v1/tailwind.config.ts
+++ saas_landingpage_v1/tailwind.config.ts
@@ -1,18 +1,23 @@
-import type { Config } from "tailwindcss";
+// tailwind.config.js
+const { type } = require("tailwindcss");
 
-export default {
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{html,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        darkBlue: "#003366",
+        brightYellow: "#FFC107",
+        softPink: "#FF4081",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
